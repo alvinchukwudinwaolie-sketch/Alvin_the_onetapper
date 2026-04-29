c527ef863774b2510f657f6f6f5a1129e2bcdaf2
alvinchukwudinwaolie-sketch <alvinchukwudinwaolie@gmail.com>
2026-04-29 13:17:32 +0100

    Add files via upload

---
+++ page.js
@@ -0,0 +1,12 @@
+const events_btn = document.getElementById('EVENTS_BTN');
+const btn_1 = document.getElementById('BTN_1');
+const btn_2 = document.getElementById('BTN_2');
+const btn_3 = document.getElementById('BTN_3');
+const logo = document.getElementById('logo');
+const experiance = document.getElementById('experiance');
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
+const subscribe = document.getElementById('subscribe');
+const home = document.getElementById('home');
+const resources = document.getElementById('resources');
+
